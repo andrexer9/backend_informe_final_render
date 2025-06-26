7d699068a77469e78b0dede32545cf39ada11a3b
andrexer9 <andrexer9@gmail.com>
2025-06-25 19:18:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,14 @@ from firebase_admin import credentials, firestore, storage
 from docxtpl import DocxTemplate
 import os
 import tempfile
-import cloudconvert
-import requests
 
 app = Flask(__name__)
 
 cred = credentials.Certificate('/etc/secrets/service_account.json')
 firebase_admin.initialize_app(cred, {
-    storageBucket: 'academico-4a053.appspot.com'
+    'storageBucket': 'academico-4a053.appspot.com'
 })
 db = firestore.client()
-cloudconvert.configure(api_key=os.environ['CLOUDCONVERT_API_KEY'])
 
 @app.route('/generar_pao', methods=['POST'])
 def generar_pao():
@@ -52,52 +49,16 @@ def generar_pao():
 
         with tempfile.TemporaryDirectory() as tmpdir:
             docx_path = os.path.join(tmpdir, f'{pao_id}.docx')
-            pdf_path = os.path.join(tmpdir, f'{pao_id}.pdf')
 
             doc.render(context)
             doc.save(docx_path)
 
-            job = cloudconvert.Job.create(payload={
-                "tasks": {
-                    "import-my-file": {
-                        "operation": "import/upload"
-                    },
-                    "convert-my-file": {
-                        "operation": "convert",
-                        "input": "import-my-file",
-                        "input_format": "docx",
-                        "output_format": "pdf"
-                    },
-                    "export-my-file": {
-                        "operation": "export/url",
-                        "input": "convert-my-file"
-                    }
-                }
-            })
-
-            upload_task = job['tasks'][0]
-            upload_url = upload_task['result']['form']['url']
-            upload_params = upload_task['result']['form']['parameters']
-
-            with open(docx_path, 'rb') as f:
-                response = requests.post(upload_url, data=upload_params, files={'file': f})
-                response.raise_for_status()
-
-            job = cloudconvert.Job.wait(id=job['id'])
-
-            export_task = [t for t in job['tasks'] if t['operation'] == 'export/url'][0]
-            pdf_url = export_task['result']['files'][0]['url']
-            pdf_content = requests.get(pdf_url).content
-
-            with open(pdf_path, 'wb') as f:
-                f.write(pdf_content)
-
             bucket = storage.bucket()
-            blob = bucket.blob(f'documentos_pao/{pao_id}.pdf')
-            blob.upload_from_filename(pdf_path)
+            blob = bucket.blob(f'documentos_pao/{pao_id}.docx')
+            blob.upload_from_filename(docx_path)
             blob.make_public()
 
-            return jsonify({'url_pdf': blob.public_url}), 200
+            return jsonify({'url_docx': blob.public_url}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -105,3 +66,4 @@ def generar_pao():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
