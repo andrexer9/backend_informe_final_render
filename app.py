0892f63a4310b64e78a9e3d656cbb7cfb8e5873f
andrexer9 <andrexer9@gmail.com>
2025-06-26 06:29:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -159,3 +159,6 @@ def generar_pao():
     except Exception as e:
         logger.error(f"Error al generar PAO: {str(e)}", exc_info=True)
         return jsonify({'error': f"Error al generar el documento: {str(e)}"}), 500
+        if __name__ == '__main__':
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
